29d3306f9cb38534d270a57e03f6a9d165bf6bb8
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-01 16:26:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return <>
+      <div className="bg-zinc-800 h-screen w-full overflow-clip">
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat, voluptatem? Animi ullam sint libero nihil harum fuga quod perspiciatis optio voluptas eius. Facere magni delectus corporis temporibus non unde ea.</h1>
+      </div>
+  
+  </>;
+}
+
+export default App;
